8d902174ab61e8e53d520c7c33df24dcdb444898
Hyung Suh <hyungjs@gmail.com>
2015-04-21 14:42:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# csslintloader for [webpack](http://webpack.github.io/)
+
+## Install
+
+```console
+$ npm install csslint-loader
+```
+
+## Usage
+
+In your webpack configuration
+
+```javascript
+module.exports = {
+  // ...
+  module: {
+    loaders: [
+      {
+        css: /\.css$/,
+        loader: "style?css!csslint")
+      },
+      {
+        scss: /\.scsss$/,
+        loader: ExtractTextPlugin.extract("style", "css!postcss!csslint!sass")
+      }
+    ]
+  }
+  // ...
+}
+```
+
+### Options
+
+You can pass the following options as a query string
+
+- configFile (default: looks for `.csslintrc` in your root directory.)
+Pass in the location/name of file if different
+
+```
+loader: "style?css?csslint?configFile=./config/cs"
+```
+
+- failOnWarning (default: true)
+If you don't pass in any config file, csslint's default setting sets all rules to 1 (warning)
+Forcing an error by default on warnings. Can disable by passing false as value
+
+```
+loader:"style?css?csslint?failOnWarning=false"
+```
---
+++ index.js
@@ -0,0 +1,85 @@
+// Dependencies
+var csslint       = require("csslint").CSSLint,
+    loaderUtils   = require("loader-utils"),
+    assign        = require("object-assign");
+
+// Helper utilities
+var formatter = require('./lib/formatter'),
+    getConfig = require('./lib/getConfig');
+
+// extract-text-plugin runs through the files again
+// adding the linted files to array to prevent duplication of linting
+var lintedFiles = [];
+// Saving default csslint rules
+var ruleSet = csslint.getRuleset();
+
+/**
+ * Css Linter
+ *
+ * @param {String|Buffer} input CSS string
+ * @param {Object} options csslint configuration
+ * @param {Object} webpack webpack instance
+ * @param {Function} callback optional callback for async loader
+ * @return {void}
+ */
+function lint(input, options, webpack, callback) {
+  var rules = getConfig(options);
+
+  // Override ruleSet with config file
+  if (rules) {
+    assign(ruleSet, rules);
+  }
+
+  // Run csslint
+  var report = csslint.verify(input, ruleSet);
+
+  // Run the formatter if there are errors/warnings and is not already linted
+  if (report.messages.length && lintedFiles.indexOf(webpack.resourcePath) === -1) {
+    // Add file to list of linted files
+    lintedFiles.push(webpack.resourcePath);
+
+    var output = formatter(report.messages, webpack.resourcePath);
+
+    if (output.errors || (options.failOnWarning == true && output.warnings)) {
+      webpack.emitError(output.message);
+    } else if (output.warnings) {
+      webpack.emitWarning(output.message);
+    }
+  }
+
+  if (callback) {
+    callback(null, input)
+  }
+}
+
+/**
+ * Webpack Loader
+ *
+ * @param {String|Buffer} input JavaScript string
+ * @returns {String|Buffer} original input
+ */
+module.exports = function(input) {
+  var options = assign(
+    {
+      configFile: './.csslintrc',
+      failOnWarning: true
+    },
+    loaderUtils.parseQuery(this.query)
+  );
+
+  this.cacheable();
+
+  var callback = this.async();
+  
+  if (!callback) { // sync
+    lint(input, options, this);
+
+    return input;
+  } else { // async
+    try {
+      lint(input, options, this, callback);
+    } catch(e) {
+      callback(e);
+    }
+  }
+}
